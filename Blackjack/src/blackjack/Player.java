f97043992fedb634840cbe63c439e6fc08a65ba2
ertay37 <ericmasontaylor@gmail.com>
2025-10-26 20:09:18 -0400

    Update Player.java

--- Blackjack/src/blackjack/Player.java
+++ Blackjack/src/blackjack/Player.java
@@ -8,10 +8,14 @@ public class Player {
   private int playerCount;
   private String cards;
 
+  // pre: m is a non-negative integer
+  // post: creates a player with money = m, empty hand, and count = 0
   public Player(int m) {
     money = m;
   }
 
+  // pre: card1 and card2 are valid Card objects, money >= 0
+  // post: creates a player with hand containing card1 and card2, updates playerCount and cards string
   public Player(Card card1, Card card2, int money) {
     this.money = money;
     this.hand = new ArrayList<>();
@@ -21,30 +25,44 @@ public class Player {
     addCard(card2);
   }
 
+  // pre: hand exists and contains valid cards
+  // post: returns playerCount as an integer
   public int getCount() {
     return playerCount;
   }
 
+  // pre: money has been initialized
+  // post: returns money
   public int getMoney() {
     return money;
   }
 
+  // pre: m >= 0
+  // post: increases money by m
   public void addMoney(int m) {
     money += m;
   }
 
+  // pre: m >= 0
+  // post: decreases money by m
   public void subtractMoney(int m) {
     money -= m;
   }
 
+  // pre: hand exists
+  // post: returns cards string
   public String getCards() {
     return cards;
   }
 
+  // pre: card is a valid Card object
+  // post: adds card to hand and updates playerCount and cards string
   public void hit(Card card) {
     addCard(card);
   }
 
+  // pre: amount >= 0
+  // post: if amount <= money, subtracts amount from money; otherwise, prints error
   public void placeBet(int amount) {
     if (amount > money) {
       System.out.println("Not enough money to place bet.");
@@ -53,6 +71,8 @@ public class Player {
     }
   }
 
+  // pre: card is a valid Card object
+  // post: appends card to hand, updates cards string, and recalculates playerCount
   public void addCard(Card card) {
     hand.add(card);
     cards += " " + card.toString();
@@ -60,6 +80,8 @@ public class Player {
     updateCount();
   }
 
+  // pre: hand contains valid cards
+  // post: updates playerCount considering Aces as 1 or 11 to avoid busting
   private void updateCount() {
     playerCount = 0;
     int aceCount = 0;
